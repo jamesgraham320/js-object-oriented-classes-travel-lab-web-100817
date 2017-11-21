f4bbd70587476335c31efbdd48d00f407f07a2c2
github username <github email address>
2017-11-21 12:58:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+	constructor(name, day) {
+		this.name = name
+		this.startDate = new Date(day)
+	}
+	
+	yearsExperienceFromBeginningOf(year) {
+		return year - this.startDate.getFullYear() -1
+	}
+	
+}
 
+class Route {
+
+	constructor(beginningLocation, endingLocation) {
+		this.beginningLocation = beginningLocation
+		this.endingLocation = endingLocation
+	}
+
+	blocksTravelled() {
+		let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+		let vert = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+		let horiz = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+		return vert + horiz
+	}
+
+	estimatedTime(isPeak) {
+		if (isPeak) {
+			return this.blocksTravelled() / 2
+		} else {
+			return this.blocksTravelled() / 3
+		}
+	}
+}
